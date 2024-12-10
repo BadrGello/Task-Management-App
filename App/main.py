13475af9d01e5ee297a0b5a29239059d9b517f62
NourrKhalled <nourrkhalled123@gmail.com>
2024-12-11 01:12:30 +0200

    Progress bars and LCDNumbers in Task Progress Tab

--- App/main.py
+++ App/main.py
@@ -6,7 +6,7 @@ from PyQt5.uic import loadUiType
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 import sys
 from os import path, makedirs
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import time
 
@@ -103,6 +103,15 @@ class mainApp(QMainWindow, FORM_CLASS):
         self.addWin = None
 
         self.events=[]
+
+
+        #Progress Tab
+        self.completion_bar_month = QProgressBar()
+        self.completion_bar_week = QProgressBar()
+        self.doneNum_month = QLCDNumber()
+        self.leftNum_month = QLCDNumber()
+        self.doneNum_week = QLCDNumber()
+        self.leftNum_week = QLCDNumber()
 
         # ScrollArea Task Widgets
         self.taskWidgetsList = [] # Widgets themselves are stored here, we iterate over them to display them
@@ -399,6 +408,9 @@ class mainApp(QMainWindow, FORM_CLASS):
         print(data)
         print("Data saved to data.json")
 
+        #For progress bar
+        self.update_progress()
+
     def loadApp(self):
         # Path to the JSON file
         file_path = path.join(self.db_folder, "data.json")
@@ -420,6 +432,9 @@ class mainApp(QMainWindow, FORM_CLASS):
         # Add task widgets with the data loaded from JSON file
         for task in self.tasksList:
             self.add_task_widget(task)
+
+        #For progress bar
+        self.update_progress()
 
     #################
 
@@ -673,7 +688,11 @@ class mainApp(QMainWindow, FORM_CLASS):
             print("ERROR, Didn't find the task to be edited")
             self.add_task_widget(task)
 
-        self.appTimer_event()    
+        self.appTimer_event() 
+
+        #For progress bar
+        self.update_progress()
+   
 
     def add_task_widget(self, task):
 
@@ -704,6 +723,9 @@ class mainApp(QMainWindow, FORM_CLASS):
         self.scrollArea_tasks.setWidgetResizable(True)
 
         # self.iterate_buttons(self)
+
+        #For progress bar
+        self.update_progress()
 
     def Handle_searchBar(self):
         self.searchBarText = self.plainTextEdit_searchTask.toPlainText()
@@ -740,6 +762,80 @@ class mainApp(QMainWindow, FORM_CLASS):
         self.Settings.show()
         refresh()
     ###################
+
+
+    
+    # Progress Tab #
+
+    def update_progress(self):
+        self.progressMonthly()
+        self.progressWeekly()
+
+    def progressMonthly(self):
+
+        """
+        Updates the progress bar for tasks completed in the current month.
+        """
+
+        self.completion_bar_month.setValue(0)
+
+
+        #Calculate current month and year
+        current_date = datetime.now()
+        current_month = current_date.month
+        current_year = current_date.year
+
+        #save current month tasks
+        current_month_tasks = [
+            task for task in self.tasksList
+            if datetime.strptime(task["date"], "%Y-%m-%d %H:%M:%S").month == current_month and
+            datetime.strptime(task["date"], "%Y-%m-%d %H:%M:%S").year == current_year
+        ]
+
+        #Calculate number of current month tasks
+        total_tasks_this_month = len(current_month_tasks)
+        completed_tasks_this_month = sum(1 for task in current_month_tasks if task["complete"])
+
+        if total_tasks_this_month > 0:
+            completion_percentage = completed_tasks_this_month / total_tasks_this_month * 100
+        else:
+            completion_percentage = 0
+            
+        self.completion_bar_month.setValue(int(completion_percentage))
+        self.doneNum_month.display(completed_tasks_this_month)
+        self.leftNum_month.display(total_tasks_this_month - completed_tasks_this_month)
+
+    def progressWeekly(self):
+        """
+        Updates the progress bar for tasks completed in the current week.
+        """
+        self.completion_bar_week.setValue(0)
+
+        current_date = datetime.now()
+        start_of_week = current_date - timedelta(days=(current_date.weekday() + 2) % 7)  # Start of week: Saterday
+
+        #save current week tasks
+        current_week_tasks = [
+            task for task in self.tasksList
+            if start_of_week <= datetime.strptime(task["date"], "%Y-%m-%d %H:%M:%S") <= current_date
+        ]
+
+        #Calculate number of current week tasks
+        total_tasks_this_week = len(current_week_tasks)
+        completed_tasks_this_week = sum(1 for task in current_week_tasks if task["complete"])
+
+        if total_tasks_this_week > 0:
+            completion_percentage = completed_tasks_this_week / total_tasks_this_week * 100
+        else:
+            completion_percentage = 0
+
+        self.completion_bar_week.setValue(int(completion_percentage))
+        self.doneNum_week.display(completed_tasks_this_week)
+        self.leftNum_week.display(total_tasks_this_week - completed_tasks_this_week)
+
+
+    ##################
+    
         
 class addWindow(QDialog, ADD_TASK_CLASS):
     #Constructor
